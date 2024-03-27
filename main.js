dfda3b88d0fc745eb178ff0ef621cb8c00b3cf9e
Elen707 <60515421+Elen707@users.noreply.github.com>
2024-03-27 20:43:55 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,273 @@
+$(document).ready(function ($) {
+    "use strict";
+
+
+    var book_table = new Swiper(".book-table-img-slider", {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        speed: 2000,
+        effect: "coverflow",
+        coverflowEffect: {
+            rotate: 3,
+            stretch: 2,
+            depth: 100,
+            modifier: 5,
+            slideShadows: false,
+        },
+        loopAdditionSlides: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+
+    var team_slider = new Swiper(".team-slider", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        speed: 2000,
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1.2,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            992: {
+                slidesPerView: 3,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+        },
+    });
+
+    jQuery(".filters").on("click", function () {
+        jQuery("#menu-dish").removeClass("bydefault_show");
+    });
+    $(function () {
+        var filterList = {
+            init: function () {
+                $("#menu-dish").mixItUp({
+                    selectors: {
+                        target: ".dish-box-wp",
+                        filter: ".filter",
+                    },
+                    animation: {
+                        effects: "fade",
+                        easing: "ease-in-out",
+                    },
+                    load: {
+                        filter: ".all, .breakfast, .lunch, .dinner",
+                    },
+                });
+            },
+        };
+        filterList.init();
+    });
+
+    jQuery(".menu-toggle").click(function () {
+        jQuery(".main-navigation").toggleClass("toggled");
+    });
+
+    jQuery(".header-menu ul li a").click(function () {
+        jQuery(".main-navigation").removeClass("toggled");
+    });
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    var elementFirst = document.querySelector('.site-header');
+    ScrollTrigger.create({
+        trigger: "body",
+        start: "30px top",
+        end: "bottom bottom",
+
+        onEnter: () => myFunction(),
+        onLeaveBack: () => myFunction(),
+    });
+
+    function myFunction() {
+        elementFirst.classList.toggle('sticky_head');
+    }
+
+    var scene = $(".js-parallax-scene").get(0);
+    var parallaxInstance = new Parallax(scene);
+
+
+});
+
+
+jQuery(window).on('load', function () {
+    $('body').removeClass('body-fixed');
+
+    //activating tab of filter
+    let targets = document.querySelectorAll(".filter");
+    let activeTab = 0;
+    let old = 0;
+    let dur = 0.4;
+    let animation;
+
+    for (let i = 0; i < targets.length; i++) {
+        targets[i].index = i;
+        targets[i].addEventListener("click", moveBar);
+    }
+
+    // initial position on first === All 
+    gsap.set(".filter-active", {
+        x: targets[0].offsetLeft,
+        width: targets[0].offsetWidth
+    });
+
+    function moveBar() {
+        if (this.index != activeTab) {
+            if (animation && animation.isActive()) {
+                animation.progress(1);
+            }
+            animation = gsap.timeline({
+                defaults: {
+                    duration: 0.4
+                }
+            });
+            old = activeTab;
+            activeTab = this.index;
+            animation.to(".filter-active", {
+                x: targets[activeTab].offsetLeft,
+                width: targets[activeTab].offsetWidth
+            });
+
+            animation.to(targets[old], {
+                color: "#0d0d25",
+                ease: "none"
+            }, 0);
+            animation.to(targets[activeTab], {
+                color: "#fff",
+                ease: "none"
+            }, 0);
+
+        }
+
+    }
+});
+
+
+
+
+
+
+
+
+
+// filter gallireyy
+
+// Select relevant HTML elements
+const filterButtons = document.querySelectorAll("#filter-buttons button");
+const filterableCards = document.querySelectorAll("#filterable-cards .card");
+
+// Function to filter cards based on filter buttons
+const filterCards = (e) => {
+    document.querySelector("#filter-buttons .active").classList.remove("active");
+    e.target.classList.add("active");
+
+    filterableCards.forEach(card => {
+        // show the card if it matches the clicked filter or show all cards if "all" filter is clicked
+        if(card.dataset.name === e.target.dataset.filter || e.target.dataset.filter === "all") {
+            return card.classList.replace("hide", "show");
+        }
+        card.classList.add("hide");
+    });
+}
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards));
+
+
+
+
+
+
+
+
+// faq
+
+
+document.querySelectorAll('.accordion-item h3').forEach((accordionToggle) => { 
+    accordionToggle.addEventListener('click', () => { 
+    const accordionItem = accordionToggle.parentNode; 
+    const accordionContent = accordionToggle.nextElementSibling; 
+  
+          // If this accordion item is already open, close it.
+     if (accordionContent.style.maxHeight) { 
+         accordionContent.style.maxHeight = null; 
+         accordionItem.classList.remove('active'); 
+        } else {
+          accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'; 
+              accordionItem.classList.add('active'); 
+          }
+      });
+  });
+
+
+
+
+
+
+//   galireyy
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+
+
+
+
+
+
+
+
+
+
+  
